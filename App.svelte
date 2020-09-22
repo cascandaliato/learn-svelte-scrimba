96978f418cd930a2ac52e44c560f27e73bd5dc54
cascandaliato <8927157+cascandaliato@users.noreply.github.com>
2020-09-22 21:24:24 +0200

    first Svelte app

---
+++ App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Face from './Face';
+  let say = 'hi';
+  setTimeout(() => {
+    say = 'bye';
+  });
+</script>
+
+<style>
+  div {
+    color: red;
+  }
+  /* :global(div) {
+    background: blue;
+  } */
+</style>
+
+<div>Say: {say}</div>
+
+<Face size={4} />
+<Face size="10" />
+<Face />
